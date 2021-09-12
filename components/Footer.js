7c3cd232931bba3bb7ac31b478f944d5cdf21eb1
Saiful <azadahmedofficial@gmail.com>
2021-09-12 22:04:28 +0600

    UI updated

---
+++ components/Footer.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 space-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800 mt-10">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb Works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb plus</p>
+        <p>Airbnb Luxe</p> 
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>It a clone!!!</p>
+        <p>Referel Accepted</p>
+        <p>Saiful's Github</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Rent</p>
+        <p>Saiful</p>
+        <p>UIU</p>
+        <p>Full stack web developer</p>
+        <p>Hire NOW!</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>for more</p>
+        <p>Follow</p>
+        <p>Subscribe</p>
+        <p>Insha Allah one day!</p>
+        <p>linkedIn</p>
+      </div>
+
+
+
+
+
+    </div>
+  );
+}
+
+export default Footer;
